38610652ee7961c8da96b498921afedd7e64d81d
zhangzhenfang <fangzhzh@gmail.com>
2016-10-31 21:43:14 +0800

    257

---
+++ 257-Binary-Tree-Paths/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+	public List<String> binaryTreePaths(TreeNode root) {
+		List<String> result;
+		Stack<TreeNode> s = new Stack<>();
+		s.push(root);
+		while(!s.empty()){
+			TreeNode cur = s.peek();
+			if(cur.left != null) {
+				
+			}
+			
+			
+
+			
+		}		
+		
+		return result;	
+	}
+}	
