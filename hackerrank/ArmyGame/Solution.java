f7fb50f566b726a579e866b996c3714bc36fad20
fangzhzh <fangzhzh@gmail.com>
2016-11-30 08:07:11 +0800

    army game

---
+++ hackerrank/ArmyGame/Solution.java
@@ -0,0 +1,11 @@
+mport java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        System.out.println((n%2+(int)(n/2))*(m%2+(int)(m/2)));
+    }
+}
