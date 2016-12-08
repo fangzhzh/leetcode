5c0388864b37a1507ed56df137290060c7d588f2
fangzhzh <fangzhzh@gmail.com>
2016-12-08 08:59:56 +0800

    move zeores, no need to keep order

---
+++ 283-move-zeroes/Solution2.java
@@ -0,0 +1,19 @@
+// no need to keep order
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        int start=0, end = nums.length-1;
+        while(start<end) {
+            if(nums[start]!=0) {
+                start++;
+                continue;
+            }
+
+            if(nums[end]==0) {
+                end--;
+                continue;
+            }
+            nums[start]=nums[end];
+            nums[end]=0;
+        }
+    }
+}
