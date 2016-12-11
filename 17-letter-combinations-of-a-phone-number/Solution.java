9d1a6e8bf9405964f4eeaf788d63903014205444
fangzhzh <fangzhzh@gmail.com>
2016-12-11 11:43:00 +0800

    backtracking

--- 17-letter-combinations-of-a-phone-number/Solution.java
+++ 17-letter-combinations-of-a-phone-number/Solution.java
@@ -1,15 +1,34 @@
+//import java.util.*;
 public class Solution {
+
+    String[] dic = {"0","1","abc", "def", "ghi", "jkl", "mno", 
+    "pqrs", "tuv", "wxyz"};
 	public List<String> letterCombinations(String digits) {
 		List<String> list = new ArrayList<>();
-		backtrack(list, new ArrayList<>(), digits, 0);
+        if(digits.length() > 0) {
+            backtrack(list,new StringBuilder() , digits, 0);
+        }
 		return list;
 	}
 
-	void backtrack(List<String> list, List<String> tmp, String digits, int start) {
-		if(tmp.size() == digits.length()) {
-			list.add(new ArrayList<>(tmp));
-		} else {
+	void backtrack(List<String> list, StringBuilder b, String digits, int start) {
+		if(b.length() == digits.length()) {
+            list.add(new String(b.toString()));
+            return;
 		}
+        for(int i = start; i < digits.length(); i++) {
+            // System.out.println(String.format("%d of digits", i));
+            String str = dic[Integer.valueOf(digits.charAt(i)-'0')];
+            for(int j = 0; j < str.length(); j++) {
+                b.append(str.charAt(j));
+                backtrack(list, b, digits, i+1);
+                b.setLength(b.length()-1);
+            }
+        }
 	}
-
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.letterCombinations("232"));
+        return ;
+    }
 }
