f6b1d7425db8f0a469bad54c6f66f18c85ab2a7d
fangzhzh <fangzhzh@gmail.com>
2016-12-11 16:02:03 +0800

    concept: isExist

--- 236-lowerst-common-ancestor-of-a-binary-tree/Solution.java
+++ 236-lowerst-common-ancestor-of-a-binary-tree/Solution.java
@@ -1,20 +1,40 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+import java.util.*;
+///**
+// * Definition for a binary tree node.
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+// */
 public class Solution {
-	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-		Stack<TreeNode> s1 = new Stack<>();
-		Stack<TreeNode> s2 = new Stack<>();
-		TreeNode cur = root;
-		s1.push(cur);
-		s2.push(cur);
-		
-	}
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        Map<TreeNode, TreeNode> map = new HashMap<>();	
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        map.put(root, null);
+        TreeNode cur;
+        while(!map.containsKey(p) || !map.containsKey(q) ) {
+            cur = queue.poll();
+            if(cur.left != null) {
+                queue.offer(cur.left);
+                map.put(cur.left, cur);
+            }
+            if(cur.right != null) {
+                queue.offer(cur.right);
+                map.put(cur.right, cur);
+            }
+        }
+        Set<TreeNode> parents = new HashSet<>();
+        while(p != null) {
+            parents.add(p);
+            p = map.get(p);
+        }
+        while(q != null && !parents.contains(q)) {
+            q = map.get(q);
+        }
+        return q;
+    }
 }
 
