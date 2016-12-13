9cf737c966efe73fe35429b9059f42a6001174a6
fangzhzh <fangzhzh@gmail.com>
2016-12-13 21:30:48 +0800

    most dubious question

--- 157-read-n-char-give-read4/Solution.java
+++ 157-read-n-char-give-read4/Solution.java
@@ -1,13 +1,17 @@
-/* The read4 API is defined in the parent class Reader4.
-      int read4(char[] buf); */
- 
 public class Solution extends Reader4 {
-    /**
-     * @param buf Destination buffer
-     * @param n   Maximum number of characters to read
-     * @return    The number of characters read
-     */
-    public int read(char[] buf, int n) {
-        
-    }
+    public int read(char[] buf, int n) {
+        char[] tmp = new char[4];
+        int cnt = 0;
+        while(cnt < n) {
+            int k = read4(tmp);
+            for(int i = 0; i < k && cnt < n; ++i) {
+                buf[cnt++] = tmp[i];
+            }
+            if(k<4) {
+                break;
+            }
+        }
+        return cnt;
+        
+    }
 }
