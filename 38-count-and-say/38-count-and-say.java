93f20577f16f7fb7027f30b161d12789daf58061
zhenfang Zhang <fangzhzh@gmail.com>
2020-09-14 23:12:01 +1200

    38-count-and-say

---
+++ 38-count-and-say/38-count-and-say.java
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode lang=java
+ *
+ * [38] Count and Say
+ */
+
+ class Solution {
+    public String countAndSay(int n) {
+        return helper("1", n-1);
+    }
+    private String helper(String num, int step) {
+        if(step == 0) {
+            return num;
+        }
+        StringBuilder result = new StringBuilder();
+        int cnt = 1;
+        char lastChar = ' ';
+
+        for(int i = 0; i < num.length(); i++) {
+            if(lastChar == ' ') {
+                lastChar = num.charAt(i);
+                cnt = 1;
+                continue;
+            } 
+            if(lastChar != num.charAt(i)) {
+                result.append(cnt);
+                result.append(lastChar);
+                lastChar = num.charAt(i);
+                cnt = 1;
+                continue;
+            }
+            cnt++;
+        }
+        result.append(cnt);
+        result.append(lastChar);
+        return helper(result.toString(),  step-1);
+    }
+}
+
+
+
+class Solution1 {
+    public String countAndSay(int n) {
+        return helper("1", n-1);
+    }
+    private String helper(String num, int step) {
+        if(step == 0) {
+            return num;
+        }
+        StringBuilder result = new StringBuilder();
+        int cnt = 0;
+
+        for(int i = 0; i < num.length(); i++) {
+            int cnt = 1;
+            int j = i+1;
+            for(; j < num.length(); j++) {
+                if(num.charAt(j)==num.charAt(i)) {
+                    cnt++;
+                    continue;
+                } 
+                break;
+            }
+            result.append(cnt);
+            result.append(num.charAt(i));
+            i = j-1;
+        }
+        return helper(result.toString(),  step-1);
+    }
+}
+
+
+/**
+ * he count-and-say sequence is the sequence of integers with the first five terms as following:
+
+1.     1
+2.     11
+3.     21
+4.     1211
+5.     111221
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+
+Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count-and-say sequence. You can do so recursively, in other words from the previous member read off the digits, counting the number of digits in groups of the same digit.
+
+Note: Each term of the sequence of integers will be represented as a string.
+ */
+
+ This question is simple in idea, and medium in implementation.
+ It's not a funcy problem but you need be 100% careful to write a write code without compiler and run.
