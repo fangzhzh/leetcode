162de18315102d6bba22c99c3621fe2eb9a5d718
fangzhzh <fangzhzh@gmail.com>
2020-09-29 05:37:32 +1300

    161-one-edit-distance

---
+++ 161-one-edit-distance/161-one-edit-distance.java
@@ -0,0 +1,69 @@
+/**
+ * Given two strings S and T, determine if they are both one edit distance apart.
+One ediit distance means doing one of these operation:
+
+insert one character in any position of S
+delete one character in S
+change one character in S to other character
+Example
+Example 1:
+
+Input: s = "aDb", t = "adb" 
+Output: true
+Example 2:
+
+Input: s = "ab", t = "ab" 
+Output: false
+Explanation:
+s=t ,so they aren't one edit distance apart
+ */
+
+/**
+ * O(max(m,n))
+ *  Idea is very simple, make use of the question information.
+ * - one edit away
+ * - add, delete, replace
+ * - no extra string created
+ * 
+ * But one more info can be used, the lengths of two strings
+ * if(m == n) {
+ * } else {
+ * }
+ * */ 
+public class Solution {
+    /**
+     * @param s: a string
+     * @param t: a string
+     * @return: true if they are both one edit distance apart or false
+     */
+    public boolean isOneEditDistance(String s, String t) {
+        if(s.compareTo(t)==0) {
+            return false;
+        }
+        int m = s.length(), n = t.length();
+        if(Math.abs(m-n) > 1) {
+            return false;
+        }
+        int i = 0, j = 0;
+        
+        for(;;i++,j++) {
+            if(i==m-1 || j == n-1) {
+                return true;
+            }
+            if(s.charAt(i)==t.charAt(j)) {
+                continue;
+            }
+            if(s.substring(i+1).compareTo(t.substring(j))==0) {
+                return true;
+            }
+            if(s.substring(i+1).compareTo(t.substring(j+1))==0) {
+                return true;
+            }
+            if(s.substring(i).compareTo(t.substring(j+1))==0) {
+                return true;
+            }
+            return false;
+        }
+        // write your code here
+    }
+}
