66b651bf91b9a9af9cdbda332117ae52bccc3597
fangzhzh <fangzhzh@gmail.com>
2021-03-15 07:53:04 +1300

    ordered collection

---
+++ 295.find-median-from-data-stream/295.find-median-from-data-stream.java
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode id=295 lang=java
+ *
+ * [295] Find Median from Data Stream
+ */
+
+/**
+Category	Difficulty	Likes	Dislikes
+algorithms	Hard (45.58%)	3868	72
+Tags
+Companies
+The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value and the median is the mean of the two middle values.
+
+For example, for arr = [2,3,4], the median is 3.
+For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+Implement the MedianFinder class:
+
+MedianFinder() initializes the MedianFinder object.
+void addNum(int num) adds the integer num from the data stream to the data structure.
+double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+ 
+
+Example 1:
+
+Input
+["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+[[], [1], [2], [], [3], []]
+Output
+[null, null, null, 1.5, null, 2.0]
+
+Explanation
+MedianFinder medianFinder = new MedianFinder();
+medianFinder.addNum(1);    // arr = [1]
+medianFinder.addNum(2);    // arr = [1, 2]
+medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+medianFinder.addNum(3);    // arr[1, 2, 3]
+medianFinder.findMedian(); // return 2.0
+ 
+
+Constraints:
+
+-105 <= num <= 105
+There will be at least one element in the data structure before calling findMedian.
+At most 5 * 104 calls will be made to addNum and findMedian.
+ 
+
+Follow up:
+
+If all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+If 99% of all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+ */
+// @lc code=start
+class MedianFinder {
+    private PriorityQueue<Integer> large;
+    private PriorityQueue<Integer> small;
+    /** initialize your data structure here. */
+    public MedianFinder() {
+        large = new PriorityQueue<>();
+        small = new PriorityQueue<>((a,b) -> {
+            return b - a;
+        });
+    }
+    
+    public void addNum(int num) {
+        if(large.size() > small.size()) {
+            large.offer(num);
+            small.offer(large.poll());
+        } else {
+            small.offer(num);
+            large.offer(small.poll());
+        }
+    }
+    
+    public double findMedian() {
+        if(large.size() > small.size()) {
+            return large.peek();
+        } else if(large.size() < small.size()) {
+            return small.peek();
+        } else {
+            return (large.peek() + small.peek()) / 2.0;
+        }
+        
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
+// @lc code=end
+
