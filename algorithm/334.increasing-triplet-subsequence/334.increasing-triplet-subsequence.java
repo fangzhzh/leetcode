2903545aed25fd559c927e8c41cc9f6d8565f616
zhenfang Zhang <fangzhzh@gmail.com>
2022-01-13 16:22:58 +1300

    first greedy proof

--- algorithm/334.increasing-triplet-subsequence/334.increasing-triplet-subsequence.java
+++ algorithm/334.increasing-triplet-subsequence/334.increasing-triplet-subsequence.java
@@ -86,3 +86,57 @@ class Solution {
         return false;
     }
 }
+
+//2022-01-13 16:22:15
+
+/*
+ * @lc app=leetcode id=334 lang=java
+ *
+ * [334] Increasing Triplet Subsequence
+ */
+
+// @lc code=start
+class Solution {
+    // 贪心
+    public boolean increasingTriplet(int[] nums) {
+        return increasingTripletGreedy(nums);
+    }
+    // greedy
+    // 我们假设三元序列 [first, second, num]
+    // 我们贪心的思想是**尽量让first, second最小**，同时满足first < second' < num
+
+    // 证明: 假设(first, second, num) 是一个递增三元序列，如果存在一个second', 且second'满足 
+    // first < second' < second，并且second'位于first和num的下标之间。因为(first, second', num)也是一个递增三元序列
+
+    // 但是当(first, seoncd', num)是一个递增三元序列的时候，也满足first < second' < second,
+    // num未必大于second,因为(first, second, nun)未必是一个递增序列
+    // 因此为了找到递增三元序列，第二个数应该尽可能小
+
+    // 同理证明，第一个数也要尽可能小
+
+
+    // 实现的说明
+    // 如果遍历过程中，如果num<=first, 则num替换first，有可能更新后的fist在second的后边，但是在second前边，必然有一个first‘满足first' < second
+    // 如果num<=second,num就是替换second
+    // 否则，num > second，我们找到了一个递增三元组
+
+    public boolean increasingTripletGreedy(int[] nums) {
+        int first = nums[0];
+        int second = Integer.MAX_VALUE;
+        int n = nums.length;
+        for(int i = 1; i < n; i++) {
+            int num = nums[i];
+            if(num <= first) {
+                first = num;
+            } else if(num <= second) {
+                second = num;
+            } else {
+                return true;
+            }
+        }
+        return false;
+
+    }
+}
+// @lc code=end
+
