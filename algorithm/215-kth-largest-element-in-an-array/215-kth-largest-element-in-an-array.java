aba3445b40dce7f2f443efc5df93d77dfc0350e6
zhangzhenfang <fangzhzh@gmail.com>
2025-02-05 17:12:20 +0800

    Update 215-kth-largest-element-in-an-array.java

--- algorithm/215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ algorithm/215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -116,3 +116,18 @@ class Solution {
         nums[j] = tmp;
     }
 }
+
+/* priority queue */
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(k);
+        for(int num: nums) {
+            minHeap.offer(num);
+            if(minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+        return minHeap.peek();
+    }
+}
