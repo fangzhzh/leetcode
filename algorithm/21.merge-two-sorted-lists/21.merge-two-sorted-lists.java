1311038a93850e75e4274a36ce67e63c1a77caeb
zhenfang Zhang <fangzhzh@gmail.com>
2025-02-12 08:48:51 +0800

    list deep understanding

--- algorithm/21.merge-two-sorted-lists/21.merge-two-sorted-lists.java
+++ algorithm/21.merge-two-sorted-lists/21.merge-two-sorted-lists.java
@@ -2,6 +2,34 @@
  * @lc app=leetcode id=21 lang=java
  *
  * [21] Merge Two Sorted Lists
+ * You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+ 
+
+Example 1:
+
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.
  */
 
 // @lc code=start
@@ -15,9 +43,18 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+/**
+ * This problem is very different from [206-reverse-linked-list](./206-reverse-linked-list)
+ * The reason are
+ * 1. There is no tail recursive opportunity
+ * 2. Need a tmp head node
+ * 3. Two pointers go one by one, instead of two pointers go together
+ * 
+ */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-            // method 1: recursive Time O(n), n is the total nodes of two lists, space O(n), no extra space, but the stack
+       
+    // method 1: recursive Time O(n), n is the total nodes of two lists, space O(n), no extra space, but the stack
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if(l1 == null) return l2;
         if(l2 == null) return l1;
@@ -31,7 +68,7 @@ class Solution {
             return l2;
         }
     }
-    // method2: non recursive
+    // method2: iterative
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode head = new ListNode(), cur = head;
         while(l1 != null || l2 != null) {
