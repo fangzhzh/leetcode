9087886c8c8466d89a2bfed05bce015de13ec2d9
zhenfang Zhang <fangzhzh@gmail.com>
2025-03-16 14:20:31 +0800

    dp, dfs, grid, binary search

--- algorithm/10.regular-expression-matching/10.regular-expression-matching.java
+++ algorithm/10.regular-expression-matching/10.regular-expression-matching.java
@@ -62,5 +62,36 @@ class Solution {
         }
     }
 }
+
+// version 3, the dp version
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        boolean[][] dp = new boolean[m+1][n+1];
+        dp[0][0] = true;
+        for(int i = 1; i <= n; i++ ) {
+            if(p.charAt(i-1) == '*') {
+                dp[0][i] = dp[0][i-2];
+            }
+        }
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                char pj = p.charAt(j-1);
+                if(pj == '*') {
+                    dp[i][j] |= dp[i][j-2];
+
+                    if(p.charAt(j-2) == '.' || p.charAt(j-2) == s.charAt(i-1)) {
+                        dp[i][j] |= dp[i-1][j];
+                    }
+                } else {
+                    if(pj == '.' || pj == s.charAt(i-1)) {
+                        dp[i][j] = dp[i-1][j-1];
+                    }
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
 // @lc code=end
 
